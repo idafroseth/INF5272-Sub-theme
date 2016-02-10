4a9e70f8fa641c567095f33c230639f7cd89ef73
idafroseth <idafroseth@gmail.com>
2016-02-10 22:43:39 +0100

    Adding font awsome and google material icons

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -60,6 +60,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Megrim' rel='stylesheet' type='text/css'>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
